9663ed85d75bf10bda18b1d8c67e2b47089b52f9
Ankit Chheda <anchheda@microsoft.com>
2015-09-28 16:49:17 -0700

    Adding AmazonProvisioning Sample

---
+++ AmazonProvisioning/ProvisioningAgent/IAmazonWebServicesIdentityAnchoringBehavior.cs
@@ -0,0 +1,24 @@
+﻿//------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+//------------------------------------------------------------
+
+namespace Samples
+{
+    using System.Threading.Tasks;
+    using Amazon.IdentityManagement;
+    using Amazon.IdentityManagement.Model;
+
+    public interface IAmazonWebServicesIdentityAnchoringBehavior
+    {
+        string Identify(Group group);
+        string Identify(User user);
+
+        Task<Group> RetrieveGroup(
+            string identifier,
+            IAmazonIdentityManagementService proxy);
+        
+        Task<User> RetrieveUser(
+            string identifier,
+            IAmazonIdentityManagementService proxy);
+    }
+}
---
+++ AmazonProvisioning/ProvisioningAgent/WebService.cs
@@ -0,0 +1,60 @@
+﻿//------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+//------------------------------------------------------------
+
+namespace Samples
+{
+    using System;
+    using Microsoft.SystemForCrossDomainIdentityManagement;
+
+    public class WebService : Service
+    {
+        private const string ArgumentNameMonitoringBehavior = "monitoringBehavior";
+        private const string ArgumentNameProviderBehavior = "providerBehavior";
+
+        private IMonitor monitor;
+        private IProvider provider;
+
+        public WebService(IMonitor monitoringBehavior, IProvider providerBehavior)
+        {
+            if (null == monitoringBehavior)
+            {
+                throw new ArgumentNullException(WebService.ArgumentNameMonitoringBehavior);
+            }
+
+            if (null == providerBehavior)
+            {
+                throw new ArgumentNullException(WebService.ArgumentNameProviderBehavior);
+            }
+
+            this.monitor = monitoringBehavior;
+            this.provider = providerBehavior;
+        }
+
+        public override IMonitor MonitoringBehavior
+        {
+            get
+            {
+                return this.monitor;
+            }
+
+            set
+            {
+                this.monitor = value;
+            }
+        }
+
+        public override IProvider ProviderBehavior
+        {
+            get
+            {
+                return this.provider;
+            }
+
+            set
+            {
+                this.provider = value;
+            }
+        }
+    }
+}
